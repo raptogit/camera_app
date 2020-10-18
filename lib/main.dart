e38be3fc430a4400c5d69480a6b9022cbfc7ef79
raptoservices <57556559+raptoservices@users.noreply.github.com>
2020-10-18 19:02:07 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:camera_app/screens/camera_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.black));
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.black,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: CameraScreen(),
+    );
+  }
+}
